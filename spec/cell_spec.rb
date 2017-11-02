db321a69c6a95000cb1ef0ed1e85e121e50966fe
ComaToastUK <jesusmanyouatethedog@gmail.com>
2017-11-02 11:40:45 +0000

    second passing test

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -1,10 +1,15 @@
 require './lib/cell.rb'
 
 RSpec.describe Cell do
-  subject(:cell) { described_class.new }
   describe '#initialize' do
     it "initializes with a value of '' " do
+      cell = Cell.new
       expect(cell.value).to eq('')
+    end
+
+    it "can be initialized with a value of 'X' " do
+      cell = Cell.new('X')
+      expect(cell.value).to eq('X')
     end
  end
 end
