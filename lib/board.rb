485ac359d62e995a67fef9b8ba4e2eb73507f404
ComaToastUK <jesusmanyouatethedog@gmail.com>
2017-11-02 13:14:16 +0000

    adding additional test for the grid and board class

--- lib/board.rb
+++ lib/board.rb
@@ -4,12 +4,16 @@ class Board
 
 attr_accessor :grid
 
-  def initialize
-    @grid = create_grid
+  def initialize(default_grid = create_grid)
+    @grid = default_grid
   end
 
   def create_grid
     Array.new(3) { Array.new(3) { Cell.new } }
+  end
+
+  def cell_selector(x,y)
+    @grid[y,x]
   end
 
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,13 +2,25 @@ require './lib/board'
 
 RSpec.describe Board do
   describe '#initialize' do
-    it 'initializes with a 2d array of cells' do
+
+    it 'initializes with rows of 3' do
       board = Board.new
       board.grid.each do |row|
       expect(row.size).to eq(3)
      end
     end
+
+    it 'initializes with 3 rows' do
+      board = Board.new
+      expect(board.grid.length).to eq(3)
+    end
+
+    it 'can be initialized with different configurations' do
+      board = Board.new(['foo','bar'])
+      expect(board.grid).to eq(['foo','bar'])
+    end
   end
+
   describe '#grid' do
     it 'is populated by Cells by default' do
       board = Board.new
@@ -16,4 +28,6 @@ RSpec.describe Board do
       expect(board.grid[randomCell][randomCell]).to be_a(Cell)
     end
   end
+
+
 end
